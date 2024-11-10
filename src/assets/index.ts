88bfffb934660aa2b3251974f285a78a09a2d2b1
unknown <ndeoswaldnji@gmial.com>
2024-11-10 01:29:50 +0100

    fix image not deploying

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,17 +1,26 @@
+import exclamation from "../../public/svg/exclamation.svg";
+import background from "../../public/images/game_background.png";
+import bits from "../../public/svg/Frame-111.svg";
+import energy from "../../public/svg/Frame-101.svg";
+import bait from "../../public/svg/Frame-109.svg";
+import time from "../../public/svg/Frame-105.svg";
+import coins from "../../public/svg/Coins.svg";
+import stack_of_coins from "../../public/svg/Stack-of-Coins.svg";
+import share from "../../public/svg/Share.svg";
+import die from "../../public/svg/die.svg";
+import locksvg from "../../public/svg/lock-svg.svg";
+import code from "../../public/images/code.png";
 export const images = {
-    exclamation:"../../public/svg/exclamation.svg",
-    background:"../../public/images/game_background.png",
-    bits:"../../public/svg/Frame-111.svg",
-    energy:"../../public/svg/Frame-101.svg",
-    bait:"../../public/svg/Frame-109.svg",
-    time:"../../public/svg/Frame-105.svg",
-    coins:"../../public/svg/Coins.svg",
-    stack_of_coins:"../../public/svg/Stack-of-Coins.svg",
-    share:"../../public/svg/Share.svg",
-    die:"../../public/svg/die.svg",
-    locksvg:"../../public/svg/lock-svg.svg",
-    code:"../../public/images/code.png",
-
-
-    
-}
+  exclamation,
+  background,
+  bits,
+  energy,
+  bait,
+  time,
+  coins,
+  stack_of_coins,
+  share,
+  die,
+  locksvg,
+  code,
+};
